5185b815286624ae45f0d0a386aecc70cba19d7b
Ryan <ryandenbleyker@gmail.com>
2023-07-20 17:36:29 -0400

    Add basic math functions in script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-
+let numberInDisplay;
+const displayWindow = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => number.addEventListener('click', displayNumber));
 
-const displayWindow = document.querySelector('.display');
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', add);
 
 function displayNumber() {
     let text = this.innerHTML;
     displayWindow.textContent += text;
+    numberInDisplay += text;
+    console.log(numberInDisplay);
+}
+
+function add(first, second) {
+    return first + second;
+}
+
+function subtract(first, second) {
+    return first - second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
 }
